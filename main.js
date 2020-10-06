0eb0ba4b6bbfec8d27a7644e358ac6837a0a1453
Gary-Ihar <yakuzawot6@gmail.com>
2020-10-06 13:02:07 +0300

    update
    ctrl+enter

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 const form = document.querySelectorAll("form"),
   windowChatUser = document.querySelectorAll(".window"),
   textarea = document.querySelectorAll("textarea");
-form.forEach((form) =>
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
 
+form.forEach((form) => {
+  let id = "";
+  form.addEventListener("submit", (e) => {
+    id = e.target.id;
+    e.preventDefault();
     if (form.firstChild.nextSibling.value !== "") {
-      render(form.firstChild.nextSibling.value.split(/\n/g), e.target.id); // теперь передам массив разбитый по enter
+      render(form.firstChild.nextSibling.value.split(/\n/g), id); // теперь передам массив разбитый по enter
       textarea.forEach((textarea) => (textarea.value = ""));
     }
-  })
-);
+  });
+  //-----------------Ctrl+Enter----------------------
+  const pressKey = new Set();
+  const keys = ["Control", "Enter"];
+  form.addEventListener("keydown", (e) => {
+    pressKey.add(e.key);
+    for (let code of keys) {
+      if (!pressKey.has(code)) {
+        return;
+      }
+    }
+    pressKey.clear();
+    render(form.firstChild.nextSibling.value.split(/\n/g), id);
+    textarea.forEach((textarea) => (textarea.value = ""));
+  });
+  form.addEventListener("keyup", (e) => {
+    pressKey.delete(e.code);
+  });
+});
+
 function render(message, userId) {
   if (message.length === 1) {
     // если абзацев нет, то все штатно
